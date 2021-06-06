396915cb4c5a5817eaf5c59f30ca051162f44d17
stonefeld - Theo Stanfield <theo.stanfield@gmail.com>
2021-06-06 13:07:04 -0300

    Changed default fontfamily

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 /* static char *font = "SauceCodePro Nerd Font:pixelsize=12:antialias=true:autohint=true"; */
-static char *font = "SauceCodePro Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=10:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
@@ -36,7 +36,7 @@ static float chscale = 1.0;
  *
  * More advanced example: L" `'\"()[]{}"
  */
-wchar_t *worddelimiters = L" ";
+wchar_t *worddelimiters = L" `'\"()[]{}";
 
 /* selection timeouts (in milliseconds) */
 static unsigned int doubleclicktimeout = 300;
