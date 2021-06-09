85cfc36800584806521920d4047fc358663654dd
stonefeld - Theo Stanfield <theo.stanfield@gmail.com>
2021-06-09 01:10:04 -0300

    Added default st colors, to change them only on the xresources file

--- config.def.h
+++ config.def.h
@@ -100,28 +100,28 @@ float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-    "#3b4252",
-    "#bf616a",
-    "#a3be8c",
-    "#ebcb8b",
-    "#81a1c1",
-    "#b48ead",
-    "#88c0d0",
-    "#e5e9f0",
+    /* 8 normal colors */
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
 	/* 8 bright colors */
-    "#4c566a",
-    "#bf616a",
-    "#a3be8c",
-    "#ebcb8b",
-    "#81a1c1",
-    "#b48ead",
-    "#8fbcbb",
-    "#eceff4",
-	[255] = 0,
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
+	[255] - 0,
 	/* more colors can be added after 255 to use with DefaultXX */
-    "#d8dee9",
-    "#2e3440",
+	"#ffffff",
+	"#000000"
 };
 
 
